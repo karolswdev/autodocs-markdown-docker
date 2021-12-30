99575fec42785baf05b02db9e0b24f29650ac73f
Karol <karolsane@gmail.com>
2021-12-29 19:42:56 -0700

    Removed interactive running of docker.

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -v $(pwd):/data -it karolswdev/autodocs-markdown-docker -c code-blocks -o test.md
+docker run -v $(pwd):/data karolswdev/autodocs-markdown-docker -c code-blocks -o test.md
 # Read the now-modified test.md to see if it populated the code file.
 cat test.md | grep -c TestExample
