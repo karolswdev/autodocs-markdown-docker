0e7a4c0467bb9c01b1fc7ea2c1cca51fa17e999f
Karol <karolsane@gmail.com>
2021-12-29 19:45:02 -0700

    Changed code-blocks to code-block

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -v $(pwd):/data karolswdev/autodocs-markdown-docker -c code-blocks -o test.md
+docker run -v $(pwd):/data karolswdev/autodocs-markdown-docker -c code-block -o test.md
 # Read the now-modified test.md to see if it populated the code file.
+cat test.md
 cat test.md | grep -c TestExample
